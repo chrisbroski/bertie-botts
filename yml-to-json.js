b97c8f18ef071d93bedc90c4969314be8c58a230
Chris Broski <chrisbroski@gmail.com>
2018-05-12 19:56:47 -0400

    initial commit

---
+++ yml-to-json.js
@@ -0,0 +1,9 @@
+const yaml = require('js-yaml');
+const fs = require('fs');
+
+fs.readFile('flavors.yml', function (err, data) {
+    const jsonData = yaml.safeLoad(data);
+    console.log(jsonData[Object.keys(jsonData)[0]].length  + " flavors so far");
+
+    fs.writeFile('flavors.json', JSON.stringify(jsonData), function () {});
+});
