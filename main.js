2d24b27ec6a86d5fa9c0d70f147dc4bebab48e5f
Chris Broski <chrisbroski@gmail.com>
2018-06-24 09:45:01 -0400

    used es6 template strings

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+/* jshint esversion: 6 */
+
 var data,
     xhr = new XMLHttpRequest(),
     lineCount = 0,
@@ -16,7 +18,7 @@ xhr.onload = function (e) {
         data.names = JSON.parse(xhr2.response);
         play();
         document.getElementById("open").style.display = "inline-block";
-    }
+    };
     xhr2.send();
 };
 xhr.send();
@@ -36,11 +38,14 @@ String.prototype.format = function () {
     });
 };
 
-String.prototype.capitalize = function () {
+String.prototype.capitalize = function (cap) {
     if (!this) {
         return "";
     }
-    return this.slice(0, 1).toUpperCase() + this.slice(1);
+    if (cap) {
+        return this.slice(0, 1).toUpperCase() + this.slice(1);
+    }
+    return this;
 };
 
 // https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript-jquery#answer-7616484
@@ -63,7 +68,7 @@ function lrng(seed) {
 }
 
 function total(flavor) {
-    var tastes = flavor[Object.keys(flavor)[0]]
+    var tastes = flavor[Object.keys(flavor)[0]];
     return Object.keys(tastes).reduce(function (a, b) {
         return a + tastes[b];
     }, 0);
@@ -73,7 +78,7 @@ function showHideButtons(buttonToShow) {
     var buttons = document.querySelectorAll("fieldset p"), ii, display;
     for (ii = 0; ii < buttons.length; ii += 1) {
         display = (buttons[ii].id === buttonToShow) ? "inline-block" : "none";
-        buttons[ii].style.display = display
+        buttons[ii].style.display = display;
     }
 }
 
@@ -100,7 +105,7 @@ function desc(flavor) {
         return ". It was terribly too salty";
     }
     if (tastes.savory > 1) {
-        return [". That was quite good", ". Mmm, that was satisfyingly savory"].rand();
+        return [". It was quite good", ". Mmm, that was satisfyingly savory"].rand();
     }
 
     if (total > 4) {
@@ -179,24 +184,22 @@ function write(flavor) {
 
     if (lineCount >= beanCount - 1) {
         main.appendChild(document.createElement("p"));
-        startwriting("The last bean in the box was {0}-flavored{1}. {2} looked forward to eating more".format(
-            beanName, description, student.given
-        ));
+        startwriting(`The last bean in the box was ${beanName}-flavored${description}. ${student.given} looked forward to eating more`);
         showHideButtons("play");
         return;
     }
     if (lineCount === 0) {
-        text = ". The first bean tasted like "
+        text = ". The first bean tasted like ";
     }
     if (lineCount === 1) {
-        text = ". {0} dared to try another. This one tasted like ".format(student.given);
+        text = `. ${student.given} dared to try another. This one tasted like `;
     }
     if (lineCount === 2) {
         main.appendChild(document.createElement("p"));
-        text = "Undeterred, {0} dug into the box again. The next one tasted like ".format(pronoun());
+        text = `Undeterred, ${pronoun()} dug into the box again. The next one tasted like `;
     }
     if (lineCount === 3) {
-        text = ". {0} tasted beans the flavor of ".format(student.given);
+        text = `. ${student.given} tasted beans the flavor of `;
         description = "";
     }
     if (lineCount > 3) {
@@ -209,10 +212,7 @@ function write(flavor) {
     if (putrid > 1) {
         main.appendChild(document.createElement("p"));
 
-        startwriting("Ugh! That one was {0}-flavored. {1} spit it out and is done eating Bertie Bott's for a long while".format(
-            beanName,
-            student.given
-        ));
+        startwriting(`Ugh! That one was ${beanName}-flavored. ${student.given} spit it out and is done eating Bertie Bott's for a long while`);
         showHideButtons("play");
     } else {
         startwriting(text + beanName + description);
@@ -228,35 +228,26 @@ function isMale() {
     return 0;
 }
 
-function pronoun(capitalize) {
-    var pronouns = ["she", "he"];
-
-    if (capitalize) {
-        return pronouns[isMale()].capitalize();
-    }
-    return pronouns[isMale()];
+function pronoun(cap) {
+    return ["she", "he"][isMale()].capitalize(cap);
 }
 
-function possessive(capitalize) {
-    var pronouns = ["her", "his"];
+function pronoun2(cap) {
+    return ["her", "him"][isMale()].capitalize(cap);
+}
 
-    if (capitalize) {
-        return pronouns[isMale()].capitalize();
-    }
-    return pronouns[isMale()];
+function possessive(cap) {
+    return ["her", "his"][isMale()].capitalize(cap);
 }
 
 function openBag() {
     showHideButtons("eat");
-    startwriting(". {0} opened the box and searched for the most delicious-looking color".format(pronoun(true)));
+    startwriting(`. ${pronoun(true)} opened the box and searched for the most delicious-looking color`);
 }
 
 function end() {
     document.querySelector("main").appendChild(document.createElement("p"));
-    startwriting("{0} closed the box. Maybe {1}'ll try more later".format(
-        student.given,
-        pronoun()
-    ));
+    startwriting(`${student.given} closed the box. Maybe ${pronoun()}'ll try more later`);
     showHideButtons("play");
 }
 
@@ -274,14 +265,7 @@ function play() {
     main.innerHTML = "";
     lineCount = 0;
     showHideButtons("open");
-    p.textContent = "{0} {1} waited for {2} first delivery at Hogwarts. {3} gently removed the package from the owl's leg, then tore into the brown paper. Inside was a {4} box of {5} Bertie Bott's Every Flavor Beans".format(
-        student.given,
-        student.sur,
-        possessive(),
-        pronoun(true),
-        sizes[parseInt(beanCount, 10)],
-        beanCount
-    );
+    p.textContent = `${student.given} ${student.sur} looked up from ${possessive()} breakfast as delivery owls streamed through the windows of Hogwart's Great Hall. A large tawny landed on the table next to ${pronoun2()} and extended its leg. ${student.given} gently removed the package, then tore into the brown paper. Inside was a ${sizes[parseInt(beanCount, 10)]} box of ${beanCount} Bertie Bott's Every Flavor Beans`;
     main.appendChild(p);
 }
 
@@ -289,11 +273,7 @@ function setHouse() {
     var className = location.hash,
         html = document.querySelector("html");
 
-    if (className) {
-        html.className = className.slice(1);
-    } else {
-        html.className = "";
-    }
+    html.className = (className) ? className.slice(1) : "";
 }
 window.onhashchange = setHouse;
 window.onload = function () {
@@ -301,7 +281,7 @@ window.onload = function () {
         document.querySelector("select").value = location.hash.slice(1);
     }
     setHouse();
-}
+};
 document.querySelector("select").onchange = function () {
     location.href = "#" + this.value;
 };
